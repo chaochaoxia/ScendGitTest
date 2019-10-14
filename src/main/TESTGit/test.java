e386035093746ce67980f38dd47b9afb3d77b68e
lic_glb <lic_glb@lcyr.com>
2019-10-14 16:27:25 +0800

    测试提交到本地库

--- src/main/TESTGit/test.java
+++ src/main/TESTGit/test.java
@@ -3,5 +3,6 @@ package TESTGit;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("再加一个功能");
     }
 }
---
+++ src/main/TESTGit/test2.java
@@ -0,0 +1,7 @@
+package TESTGit;
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("新增");
+    }
+}
